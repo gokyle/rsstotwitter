0bed517d841429d02a81f97a2e9a0f1403e628d3
Kyle Isom <coder@kyleisom.net>
2012-09-03 00:42:13 -0600

    update heroku setup script

--- heroku_setup.sh
+++ heroku_setup.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
 
+if [ -z "$1" ]; then
+    echo "[!] please pass the environment setup script on the commandline."
+    exit 1
+fi
+
+if [ -z "${GOPATH}" ]; then
+    echo "[!] GOPATH needs to be set."
+    exit 1
+fi
+
+echo "[+] creating heroku app"
+heroku create
+. ./$1
+
 if [ -z "${RSS_FEED}" ]; then
     echo "[!] please run the environment setup script first."
     echo "    the script won't work properly if the environment variables"
     echo "    aren't set."
     exit 1
 fi
-
-if [ -z "${GOPATH}" ]; then
-    echo "[!] GOPATH needs to set."
-    exit 1
-fi
-
-echo "[+] creating heroku app"
-heroku create
 
 echo "[+] adding Go buildpack"
 heroku config:add BUILDPACK_URL=git://github.com/kr/heroku-buildpack-go.git
